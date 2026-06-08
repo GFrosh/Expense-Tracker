da9617c75606941a0f9a8da29a7c5e6cba2753ae
Gideon Onyegbula <gigi.onyegbula.2007@gmail.com>
2026-06-08 22:19:41 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const app = express();
 app.use(express.static(path.join(__dirname, "client")));
 app.listen(3000, () => {
